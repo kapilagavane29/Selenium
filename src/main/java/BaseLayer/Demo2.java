cc8e429c63e77fdf65af2233f80b7d86d305071e
Kapil Gavane <kapilgavanekg@gmail.com>
2022-05-12 12:45:38 +0530

    new file is added

--- src/main/java/BaseLayer/Demo2.java
+++ src/main/java/BaseLayer/Demo2.java
@@ -2,6 +2,11 @@ package BaseLayer;
 
 public class Demo2 {
 public static void main(String[] args) {
+	System.out.println("my name is lakhsn");
+	System.out.println("my name is lakhsn");
+	System.out.println("my name is lakhsn");
+	System.out.println("my name is lakhsn");
+
 	
 }
 
